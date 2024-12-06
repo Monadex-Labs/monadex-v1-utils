abf71526063899c1a27bcbf2e62f7f024a5b6452
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-12-07 01:23:21 +0530

    feat: Rename WMND contract to WrappedMonad, add natspec, and change visibility of some functions

---
+++ src/interfaces/IWrappedMonad.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface IWrappedMonad {
+    function withdraw(uint256 _wad) external;
+
+    function totalSupply() external;
+
+    function approve(address _guy, uint256 _wad) external;
+
+    function transfer(address _dst, uint256 _wad) external;
+
+    function deposit() external;
+
+    function transferFrom(address src, address _dst, uint256 _wad) external;
+
+    function name() external pure returns (string memory);
+
+    function symbol() external pure returns (string memory);
+
+    function decimals() external pure returns (uint256);
+}
